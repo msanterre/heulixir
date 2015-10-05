d3add2fc07e5f7ef20a5ac4c6a031fc905490ded
Maxime Santerre <mooxeh@gmail.com>
2015-10-04 22:03:15 -0700

    Optimized solution for largest prime factor

--- largest_prime_factor.exs
+++ largest_prime_factor.exs
@@ -1,15 +1,64 @@
 defmodule PrimeFactor do
 
   def calculate(n) do
-    primes = 1..n |> Enum.filter &(prime?(&1))
-    primes |> Enum.filter &(rem(n, &1) == 0)
+    find(n, [])
   end
 
-  defp prime?(n) do
-    divisibles = 1..n |> Enum.filter(&(rem(n, &1)) == 0)
-    length(divisibles) == 2
+  def find(n, factors) do
+    if prime?(n) do
+      Enum.max([n | factors])
+    else
+      [rest, fact] = acc_divide(n, 2)
+      find(rest, [fact | factors])
+    end
+  end
+
+  defp acc_divide(number, divisor) when rem(number, divisor) == 0, do: [round(number/divisor), divisor]
+  defp acc_divide(number, divisor), do: acc_divide(number, next_prime(divisor))
+
+  def next_prime(n) do
+    next = n + 1
+    if prime?(next) do
+      next
+    else
+      next_prime(next)
+    end
+  end
+
+  # Simple heuristic functions for speed and love
+  def prime?(1), do: true
+  def prime?(3), do: true
+  def prime?(5), do: true
+
+  def prime?(p) do
+    r = :random.uniform(p-1)
+    t = mpow(r, p-1, p)
+
+    if t == 1 do
+      true
+    else
+      false
+    end
+  end
+
+  def mpow(n, 1, _) do
+    n
+  end
+
+  def mpow(n, k, m) do
+    mpow(rem(k, 2), n, k, m)
+  end
+
+  def mpow(0, n, k, m) do
+    x = mpow(n, div(k, 2), m)
+    rem(x*x, m)
+  end
+
+  def mpow(_, n, k, m) do
+    x = mpow(n, k-1, m)
+    rem(x*n, m)
   end
 
 end
 
-IO.inspect PrimeFactor.calculate(600851475143)
+IO.inspect PrimeFactor.calculate(600_851_475_143)
--- multiples_of_3_and_5.exs
+++ multiples_of_3_and_5.exs
@@ -1,2 +1,3 @@
-res = 1..(1000-1) |> Enum.filter(&(rem(&1, 3) == 0 || rem(&1, 5) == 0)) |> Enum.sum
-IO.inspect(res)
+1..(1000-1)
+|> Enum.filter(&(rem(&1, 3) == 0 || rem(&1, 5) == 0))
+|> Enum.sum
